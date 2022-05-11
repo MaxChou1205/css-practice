a8a5ddac3f792df63f9483225e27a2554bb65829
Max <v2asault@yahoo.com.tw>
2022-05-11 15:04:35 +0800

    control multiple image animate with js

--- pages/image-scale-carousel/main.js
+++ pages/image-scale-carousel/main.js
@@ -2,3 +2,14 @@ document.addEventListener("scroll", () => {
   const containerElement = document.querySelector(".container");
   containerElement.classList.toggle("scroll", window.scrollY > 0);
 });
+
+let active = 0;
+let inActive = 0;
+const listItems = document.querySelectorAll(".kv_item");
+
+setInterval(() => {
+  active = (active + 1) % listItems.length;
+  inActive = (active - 2 + listItems.length) % listItems.length;
+  listItems[active].classList.add("active");
+  listItems[inActive].classList.remove("active");
+}, 5000);
