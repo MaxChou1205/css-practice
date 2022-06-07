23a53ced75112e6bd6aadaa43876c6793e4117ab
Max <v2asault@yahoo.com.tw>
2022-06-07 22:55:44 +0800

    add layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
   ],
   // safelist: [/(bg|ring)-(.*)-(\d{1}0{1,2})/],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "fb-bg": "#18191a",
+        "fb-header": "#242526",
+        "fb-input": "#3a3b3c",
+        "fb-popover": "#3e4042",
+        "fb-active": "#323436",
+        "fb-card": "#242526",
+        fb: "#2e89ff"
+      }
+    }
   },
   plugins: [require("@tailwindcss/forms")]
 };
