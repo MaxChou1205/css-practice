b3d05936848075a4697e0ebdb74dc39dd6dd9d30
Max <v2asault@yahoo.com.tw>
2022-06-27 22:35:56 +0800

    try to remove auto type plugin

--- pages/auto-typing/main.js
+++ pages/auto-typing/main.js
@@ -1,11 +1,27 @@
 import "./style.scss";
-import Typed from "typed.js";
+// import Typed from "typed.js";
 
-var options = {
-  strings: ["Max Chou", "Web Developer"],
-  typeSpeed: 80,
-  backSpeed: 80,
-  loop: true
+// var options = {
+//   strings: ["Max Chou", "Web Developer"],
+//   typeSpeed: 80,
+//   backSpeed: 80,
+//   loop: true
+// };
+
+// var typed = new Typed(".auto_input", options);
+
+const typeElement = document.querySelector(".auto_input");
+const text = "Max Chou";
+
+let index = 1;
+
+const writeText = () => {
+  typeElement.innerText = text.slice(0, index);
+  index++;
+
+  if (index > text.length) index = 1;
+
+  setTimeout(writeText, 300);
 };
 
-var typed = new Typed(".auto_input", options);
+writeText();
