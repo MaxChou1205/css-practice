64b115ba712cc64d76c056f310e8157650b98409
Max <v2asault@yahoo.com.tw>
2022-07-20 21:50:44 +0800

    add feature of get files in menu automatically

--- main.js
+++ main.js
@@ -1,56 +1,11 @@
-// import "./style.css";
-// const fs = require("fs");
-// import fs from "browserify-fs";
-// var files = fs.readdirSync("./pages");
-// console.log(files);
-// const requireModule = import.meta.globEager("/pages/*");
+let files = import.meta.glob("/pages/*/*.html");
+for (let filePath in files) {
+  let paths = filePath.split("/");
 
-// for (const key in requireModule) {
-//   debugger;
-//   console.log(key);
-// }
-
-// import path from "path";
-
-// const dirname = path.dirname("./pages");
-
-// console.log(dirname);
-
-let folders = [
-  "list-hover-effects",
-  "animated-menu-toggle",
-  "custom-checkbox",
-  "animated-website",
-  "switch",
-  "text-hover-effects",
-  "sidebar-menu",
-  "responsive-navbar",
-  "progress-bar",
-  "trillo-flexbox",
-  "auto-typing",
-  "tours-website",
-  "image-scale-carousel",
-  "scroll-scale",
-  "animated-navigation-menu",
-  "facebook-ui",
-  "tailwind-pricing-page",
-  "toast-notification",
-  "form-input-wave",
-  "scroll-animation",
-  "rotating-navigation-animation",
-  "expanding-cards",
-  "gradient-clock",
-  "galaxy-animate",
-  "3d-boxes-background",
-  "netflix-mobile-navigation",
-  "button-ripple-effect"
-];
-
-folders.forEach(folderName => {
   const liElement = document.createElement("li");
   const linkElement = document.createElement("a");
-  linkElement.setAttribute("href", `./pages/${folderName}/index.html`);
-  linkElement.textContent = folderName;
+  linkElement.setAttribute("href", `.${filePath}`);
+  linkElement.textContent = paths[2];
   liElement.appendChild(linkElement);
   document.querySelector("#menu").appendChild(liElement);
-});
+}
