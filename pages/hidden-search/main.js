782bb4eb49d5d3306c4caa96224643cea9df54df
Max <v2asault@yahoo.com.tw>
2022-08-29 21:47:11 +0800

    add hidden search page

---
+++ pages/hidden-search/main.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+const search = document.querySelector('.search')
+const input = document.querySelector('input')
+const btn = document.querySelector(".btn")
+
+btn.addEventListener('click',()=>{
+    search.classList.toggle('active')
+    input.focus()
+})
