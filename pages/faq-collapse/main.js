536eeae80692bd341c201ad1f394a6f194722fc0
Max <v2asault@yahoo.com.tw>
2022-09-21 21:31:18 +0800

    add faq-collapse

---
+++ pages/faq-collapse/main.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+const buttons = document.querySelectorAll('.faq-toggle');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    button.parentNode.classList.toggle('active');
+  })
+})
