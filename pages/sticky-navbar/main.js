d44214106f1af8013223236892f8558d326f80e1
MaxChou1205 <v2asault@yahoo.com.tw>
2023-07-19 21:08:53 +0800

    add sticky navigation

---
+++ pages/sticky-navbar/main.js
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+const nav = document.querySelector(".nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+});
